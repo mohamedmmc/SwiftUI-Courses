7231bbc4969f52571189242af62929ba1431630c
mohamedmmc <mohamedmelek.chtourou@esprit.tn>
2023-01-29 16:21:35 +0100

    first commit guess the flag

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mohamed Melek Chtourou on 27/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
